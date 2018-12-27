0233435cbca8a934ca9f0e92d4649fb1221192f5
DuyKhanh <caoduykhanh406@gmail.com>
2018-12-27 20:44:42 +0700

    Bài tập

---
+++ BootcampPreparation/Session2.VariableAndDataTypes/SuDungToanTu/BT.js
@@ -0,0 +1,4 @@
+var width = parseInt(prompt("Enter the chiều rộng"));
+var height = parseInt(prompt("Enter the chiều cao"));
+var area = width * height;
+document.write("Diện Tích là: " + area);
